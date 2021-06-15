8e21266db9c333bc543d07ff3754546eac86c405
lpattis1 <lpattis1@asu.edu>
2021-06-15 10:27:23 -0500

    Added a fixed navbar on scroll.

---
+++ javascript/navbar.js
@@ -0,0 +1,13 @@
+function makeNavbarFixed() {
+  const nav = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", function (e) {
+    if (window.pageYOffset >= 250) {
+      nav.classList.add("fixed");
+    } else if (window.pageYOffset <= 150) {
+      nav.classList.remove("fixed");
+    }
+  });
+}
+
+makeNavbarFixed();
